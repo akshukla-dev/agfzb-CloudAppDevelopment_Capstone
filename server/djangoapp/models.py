48e31d04de9af520b6c16fc58711964e3e4fedfa
Amit Kumar Shukla <akshukla.dev@gmail.com>
2022-01-03 00:02:25 +0530

    User Administartion Added

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,6 +1,13 @@
-from django.db import models
+import sys
 from django.utils.timezone import now
+try:
+    from django.db import models
+except Exception:
+    print("There was an error loading django modules. Do you have django installed?")
+    sys.exit()
 
+from django.conf import settings
+import uuid
 
 # Create your models here.
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -14,12 +14,62 @@ import json
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
+# Create authentication related views
+
+
+
+
+def registration_request(request):
+    context = {}
+    if request.method == 'GET':
+        return render(request, 'djangoapp/user_registration.html', context)
+    elif request.method == 'POST':
+        # Check if user exists
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user_exist = False
+        try:
+            User.objects.get(username=username)
+            user_exist = True
+        except:
+            logger.debug("{} is new user".format(username))
+        if not user_exist:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+            login(request, user)
+            return redirect("djangoapp:about")
+        else:
+            context['message'] = "User already exists."
+            return render(request, 'djangoapp/user_registration.html', context)
+
+
+def login_request(request):
+    context = {}
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        print(user)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:about')
+        else:
+            context['message'] = "Invalid username or password."
+            return render(request, 'djangoapp/user_login.html', context)
+    else:
+        return render(request, 'djangoapp/user_login.html', context)
+
+
 
 
 # Create an `about` view to render a static about page
 # def about(request):
 # Create your views here.
+
+
+
 def about(request):
     context = {}
     # If the request method is GET
@@ -43,6 +93,14 @@ def contact(request):
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
+def logout_request(request):
+    # Get the user object based on session id in request
+    print("Log out the user `{}`".format(request.user.username))
+    # Logout user in the request
+    logout(request)
+    # Redirect user back to course list view
+    return redirect('djangoapp:about')
+
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
